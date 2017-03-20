45147007bad5543640b0e9efbb7c8087b15c39e5
soundestmammal <robertchecco@Roberts-MacBook-Air.local>
2017-03-20 01:43:53 -0400

    Variables

--- scripts.js
+++ scripts.js
@@ -1,3 +1,13 @@
 console.log("This is the beginning of the program");
-alert("This is scripts page");
+var message = "Hello!";
+alert(message);
+var name = prompt("What is your name?")
+console.log(name);
+//How to find the length of a string
+console.log(name.length);
+
+//Trying out of method. (Lowercase)
+console.log(name.toLowerCase());
+var welcome = "Welcome to JavaScript Basics";
+alert(welcome + " " + name)
 console.log("This is the end of the program");

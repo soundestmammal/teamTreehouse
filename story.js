33792d9cff12ec8e2c3230740ca4a45e1dc78b61
soundestmammal <robertchecco@Roberts-MacBook-Air.local>
2017-03-20 02:56:57 -0400

    time & story

---
+++ story.js
@@ -0,0 +1,8 @@
+var questions = 3;
+var questionsLeft = '[' + questions + ' questionsLeft]';
+var adjective = prompt("Please type an Adjetive" + questionsLeft);
+questions -= 1;
+var adjective = prompt("Please type an Verb" + questionsLeft);
+questions -= 1;
+var adjective = prompt("Please type a noun" + questionsLeft);
+var noun
---
+++ time.js
@@ -0,0 +1,8 @@
+var secondsPerMin = 60;
+var minsPerHour = 60;
+var hoursPerDay = 24;
+var daysPerWeek = 7;
+var weeksPerYear = 52;
+
+var secsPerDay = secondsPerMin * minsPerHour * hoursPerDay;
+document.write("There are " + secsPerDay + " seconds in one day.");

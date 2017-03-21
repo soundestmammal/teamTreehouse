cab7efb444bc269775b5f237b45f346d6b83c904
soundestmammal <robertchecco@Roberts-MacBook-Air.local>
2017-03-21 02:09:25 -0400

    random num

--- scripts.js
+++ scripts.js
@@ -1,13 +1,21 @@
 console.log("This is the beginning of the program");
+
 var message = "Hello!";
+
 alert(message);
+
 var name = prompt("What is your name?")
+
 console.log(name);
+
 //How to find the length of a string
 console.log(name.length);
 
 //Trying out of method. (Lowercase)
 console.log(name.toLowerCase());
+
 var welcome = "Welcome to JavaScript Basics";
+
 alert(welcome + " " + name)
+
 console.log("This is the end of the program");
